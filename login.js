937c3fa33a85acaa0b42979ce9c830598d53131f
Alexo <aortiz@iteggo.com>
2025-06-10 13:03:07 +0200

    dfafaffa

--- login.js
+++ login.js
@@ -1,4 +1,6 @@
-
+const supabaseUrl = "https://yhcivepnywbvskalfqfd.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloY2l2ZXBueXdidnNrYWxmcWZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwODA5ODgsImV4cCI6MjA2MjY1Njk4OH0.Nl2_D3j5MHTuGnIaelVrREqLqQSPE-Z8ciVnaJGVa-w";
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 // Elementos del DOM
 const loginForm = document.getElementById('login-form');
